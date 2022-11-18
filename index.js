cec90cba262508585c2bfef1e14e59b9a5581100
Steven R. Loomis <srl295@fahmu.net>
2022-11-18 15:51:15 -0600

    licensure

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
-const SwaggerClient = require('swagger-client');
+// Copyright (C) 2022 Code Hive Tx, LLC.
 
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const SwaggerClient = require('swagger-client');
 const { PORT } = require('process').env;
 const ical = require('ical-generator');
 const http = require('http');
@@ -22,13 +36,13 @@ const cli = new SwaggerClient({
     }
 });
 
-const calendar = ical({name: `Drop Calendar for ${config.drop || 'ALL'}`});
+const calendar = ical({ name: `Drop Calendar for ${config.drop || 'ALL'}` });
 const port = PORT || 3000;
 
 function createEntry() {
     const startTime = new Date();
     const endTime = new Date();
-    endTime.setHours(startTime.getHours()+1);
+    endTime.setHours(startTime.getHours() + 1);
     calendar.createEvent({
         start: startTime,
         end: endTime,
@@ -41,32 +55,32 @@ function createEntry() {
 
 function serve() {
     http.createServer((req, res) => calendar.serve(res))
-    .listen(port, '127.0.0.1', () => {
-        console.log(`Server running at http://127.0.0.1:${port}/`);
-    });    
+        .listen(port, '127.0.0.1', () => {
+            console.log(`Server running at http://127.0.0.1:${port}/`);
+        });
 }
 
 async function main() {
     const client = await cli;
 
-    if(!config.drop) {
+    if (!config.drop) {
         console.log('No drop specified.. listing');
         const drops = await client.apis.drop.findDrops();
-//        console.dir(drops.body);
+        //        console.dir(drops.body);
         return drops.body;
     }
 
     // get drop info
-    const dropInfo = await client.apis.drop.findDropById({id: config.drop});
+    const dropInfo = await client.apis.drop.findDropById({ id: config.drop });
     console.dir(dropInfo.body);
 
-    const {body: trips} = await client.apis.trip.findTrips({drop: config.drop});
+    const { body: trips } = await client.apis.trip.findTrips({ drop: config.drop });
 
-    for(const trip of trips) {
-        const {route, cutoff} = trip;
+    for (const trip of trips) {
+        const { route, cutoff } = trip;
         const startTime = new Date(cutoff);
         const endTime = new Date(cutoff);
-//        endTime.setHours(startTime.getHours()+1);
+        //        endTime.setHours(startTime.getHours()+1);
         calendar.createEvent({
             start: startTime,
             end: endTime,
@@ -80,4 +94,4 @@ async function main() {
     serve(); // setup server
 }
 
-main().then(ok => console.dir({ok}), err => console.error(err));
+main().then(ok => console.dir({ ok }), err => console.error(err));
